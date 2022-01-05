610c1c50c8e8a2b0b6081f1f9c0107dc04ce8cf6
Drew <drewhahn23@gmail.com>
2022-01-05 14:11:14 -0800

    loaded api + viz sun & 3 planets
    API now loading positional data. not feeding into planet rotation/position just yet. currently have planets rotating at const set speeds

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import LoadData from './components/LoadData';
+import GetData from './components/LoadData';
 // import './components/SceneInit';
 
 function App() {
 
   const mountRef = useRef(null);
+  const data = GetData();
+
+  console.log(data);
 
   useEffect(() => {
     
@@ -19,18 +22,55 @@ function App() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     
     document.body.appendChild( renderer.domElement );
+
+    var sun = new THREE.SphereGeometry(1);
+    var sunMaterial = new THREE.MeshBasicMaterial( {color: 0xFF0000 } );
+    var sunSphere = new THREE.Mesh( sun, sunMaterial );
+
+    scene.add( sunSphere );
+
+    var mercury = new THREE.SphereGeometry(.1);
+    var mercuryMaterial = new THREE.MeshBasicMaterial( {color: 0x808080 });
+    var mercurySphere = new THREE.Mesh( mercury, mercuryMaterial );
+
+    var venus = new THREE.SphereGeometry(.2);
+    var venusMaterial = new THREE.MeshBasicMaterial( {color: 0xFFD580 });
+    var venusSphere = new THREE.Mesh( venus, venusMaterial );
     
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    
-    scene.add( cube );
-    camera.position.z = 5;
+    var earth = new THREE.SphereGeometry(.3);
+    var earthMaterial = new THREE.MeshBasicMaterial( {color: 0xADD8E6 });
+    var earthSphere = new THREE.Mesh( earth, earthMaterial );
+
+    var pivotMercury = new THREE.Object3D();
+    var pivotVenus = new THREE.Object3D();
+    var pivotEarth = new THREE.Object3D();
+
+    mercurySphere.position.set(2,0,0);
+    venusSphere.position.set(4,0,0);
+    earthSphere.position.set(6,.4,0);
+
+
+    scene.add(pivotMercury);
+    scene.add(pivotVenus);
+    scene.add(pivotEarth);
+
+
+    pivotMercury.add(mercurySphere);
+    pivotVenus.add(venusSphere);
+    pivotEarth.add(earthSphere);
+
+
+    camera.position.z = 10;
     
     var animate = function () {
       requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+
+
+      pivotMercury.rotation.y += 0.01;
+      pivotVenus.rotation.y += 0.005;
+      pivotEarth.rotation.y += 0.0025;
+
+
       renderer.render( scene, camera );
     };
     
@@ -43,6 +83,7 @@ function App() {
       
     <ol>
       <ol>Hi</ol>
+      <GetData></GetData>
     <ol><div ref={mountRef}></div></ol>
 
 
--- src/components/LoadData.js
+++ src/components/LoadData.js
@@ -21,6 +21,34 @@ const GetData = () => {
 
     },[]);
 
+
+    return (
+        <div>
+            <h4>Planetary Coordinates With Respect to Sun</h4>
+            { loading ? (<h4>loading positional data</h4>) :
+            (Object.entries(data).map(([key,val])=> {
+
+            return (
+                
+                <ol key = {key}>
+                <ol>Planet: {key}</ol>
+                <ol>Declination: {val.declination}</ol>
+                <ol>Distance from Sun (AU): {val.distance_au_from_sun}</ol>
+                <ol>Right Ascension: {val.right_ascension}</ol>
+                <ol>X Coordinate: {val.x_coord}</ol>
+                <ol>Y Coordinate: {val.y_coord}</ol>
+                <ol>Z Coordinate: {val.z_coord}</ol>
+            </ol>
+            )
+            },
+            ))
+        }
+
+        </div>
+    );
+
+
+
 }
 
 export default GetData;
